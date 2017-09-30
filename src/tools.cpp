0021b7b81c2605b948b7383fcf9596db5eaab120
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-09-30 20:07:50 -0300

    Implement RMSE calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,27 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if(estimations.size() == 0){
+    cout << "Error: no estimations were given for rmse!" << endl;
+    return rmse;
+  }
+
+  if(estimations.size() != ground_truth.size()){
+    cout << "Error: estimation and ground truth are different sizes!" << endl;
+    return rmse;
+  }
+
+  for(int i=0; i < estimations.size(); i++){
+    VectorXd diff(estimations.size());
+    diff = estimations[i] - ground_truth[i];
+    VectorXd residuals = diff.array()*diff.array();
+    rmse += residuals;
+  }
+
+  rmse = rmse/estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
